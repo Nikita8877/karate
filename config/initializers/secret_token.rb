7c262e9a4b09a5d514d75a75bf18ecf51f16ea78
emNikita8877/em <emnikita8877@mail.ru/em>
2013-09-20 14:09:29 +0400

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Karate::Application.config.secret_token = '958ee277372da0db3bd158a4175ba1f3a36f5b7d95d9221bd5f08312f422ecf586ef1754d2f6a1a7f3e10ef839fe7cff36dd68125aaf1fbe7639d5c6477c04ff'
